ba4b2e81e026b71dcd7de93b2d53b98be44a9fb2
lucsoft <lucsoft@dev107.com>
2018-10-13 00:11:36 +0200

    Create lang.ts

---
+++ PiServer/languages/lang.ts
@@ -0,0 +1,26 @@
+export var de_de = {
+    nav: {
+        myrooms: "Meine Räume",
+        actions: "Aktionen",
+        configuation:"Konfiguration"
+    },
+    pages: [
+        {
+            id:"homesys",
+            subtitle: "Version",
+            temperatur:"Temperatur",
+            humidity:"Luftfeuchtigkeit",
+            activedevices: "Aktive Geräte",
+            yourfavorites:"Deine Favoriten",
+            yourfavoritessub:"Füg neue in der Konfiguration Hinzufügen",
+
+        }
+    ],
+    devices: {
+        light: "Licht",
+        button:"Taste",
+        door:"Tür",
+        screen:"Bildschrim"
+    }
+    
+};
