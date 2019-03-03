b4da5bcf07aba814b58d7cf741b0be5e88c98d1d
lucsoft <lucsoft@dev107.com>
2019-03-03 11:26:41 +0100

    testing

--- Mainframe/modules/lucsoft.Mainframe.js
+++ Mainframe/modules/lucsoft.Mainframe.js
@@ -32,7 +32,8 @@ mmanager.onModulesInitialized = (modules, cb) => {
 mmanager.onReady = (e) => {
     if(e.name == "lucsoft.webServer") {
         e.data.web.get('/', function (req, res) {
-            res.send('<h1>It Works!</h1>lol');
+            res.sendFile(process.cwd() + '/lib/web/login.html');
+            
         });
         e.data.web.get('/device/lamp/true', function (req,res) {
             res.send('Changed Light');
@@ -42,16 +43,37 @@ mmanager.onReady = (e) => {
             res.send('Changed Light');
             lamp.updatePower(false);
         })
+        e.data.web.get('/device/lock/lock', function (req,res) {
+            res.send('Changed Lock');
+            fakelock.updateState(true);
+        })
+        e.data.web.get('/device/lock/unlock', function (req,res) {
+            res.send('Changed Lock');
+            fakelock.updateState(false);
+        })
+        e.data.web.get('/device/sensor/detect', function (req,res) {
+            res.send('Changed Sensor');
+            motionsensor.updateState(true);
+        })
+        e.data.web.get('/device/sensor/lost', function (req,res) {
+            res.send('Changed Sensor');
+            motionsensor.updateState(false);
+        })
+        e.data.web.get('/device/sensor/toggle', function (req,res) {
+            res.send('Changed Sensor');
+            motionsensor.updateState(!motionsensor.getState());
+        })
         e.data.web.get('/device/lamp/toggle', function (req,res) {
             res.send('Changed Light');
             lamp.updatePower(!lamp.getPower());
         })
-        
-
+        e.data.web.get('/errormsg', function (req,res) {
+            res.send(errormsg);
+        })
         e.data.startWebserver();
     }
 }
-var homekit,cmdMan,lamp;
+var homekit,cmdMan,lamp,fakelock,motionsensor,errormsg;
 mmanager.onModulesAllCompleted = (e) => {
     homekit = mmanager.modules.find(x => x.name == "lucsoft.HAPWrapper").data;
     cmdMan = mmanager.modules.find(x => x.name == "lucsoft.commandManager").data;
@@ -68,5 +90,27 @@ mmanager.onModulesAllCompleted = (e) => {
             }
         } 
     });
+    fakelock = homekit.createLock({
+        displayName: "Lock",
+        serialNumber: "FL1344",
+        model: "Fake Lock",
+        locked: true,
+        onLockState: (e) => {
+        } 
+    });
+    motionsensor = homekit.createMotionSensor({
+        displayName: "Motion Sensor",
+        serialNumber: "MS1344",
+        model:"HTML Based Motion Sensor",
+    });
+    try {
+        homekit.createCustom({
+            displayName: "Motion Sensor",
+            serialNumber: "GM134434",
+            model:"HTML Based Motion Sensor",
+        });
+    } catch (error) {
+        errormsg= error;
+    }
 };
 
