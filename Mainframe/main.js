2e3a28c82dfaa9cbb2da6891c5fedb83d3f21fc7
lucsoft <mail@lucsoft.de>
2019-12-16 07:45:22 +0100

    Removed Unused Stuff (sperated code in diffrent Repos )

--- Mainframe/main.js
+++
@@ -1,8 +0,0 @@
-try {
-    require("./modules/lucsoft.Mainframe");
-    
-} catch (error) {
-    const { exec} = require('child_process');
-    exec("clear > /dev/tty1");
-    exec('echo "'+ error+'" > /dev/tty1');
-}
